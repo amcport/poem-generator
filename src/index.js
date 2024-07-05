fe4730f57226ce112820fa9c775c63fa89e7c64f
amcport <143063731+amcport@users.noreply.github.com>
2024-07-05 08:14:04 +0100

    Added the basic javascript and the typewriter plug in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: [
+      "Every night it’s the same dark dream. And it always ends with a scream",
+    ],
+    autoStart: true,
+    delay: "natural",
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
