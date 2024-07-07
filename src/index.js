b3f5f6e80caf352144dc8194a3f48a101c34e9c4
amcport <143063731+amcport@users.noreply.github.com>
2024-07-07 14:57:29 +0100

    Improve UX
    changed the html slightly by adding a hint, we improved javascript, hid poem box by default and added blinking animation

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ function generatePoem(event) {
     "You are a dark romance poet and love to write short poems- Your mission is to generate a 4 line poem in basic html format. make sure the contnt of the poem is always black in color. Include a title for the poem and always put it in a <strong> element and has a text size of 22px. Sign the poem with ' - your romantic AI poet' and always put the signature inside an <italic> element and the signature is pink in color. Make sure to follow the user instructions";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}. Please behave.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generting a Dark Romance Poem about ${instructionsInput.value}</div>`;
   //make the call to the API
   console.log("Generating Poem");
   //Display generated poem
